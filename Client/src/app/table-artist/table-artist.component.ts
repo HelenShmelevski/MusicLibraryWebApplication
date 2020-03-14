53c0fcda961659df87702430066bd29df3888964
Alina Kuzminykh <alinakuzminyhk@gmail.com>
2020-03-14 22:49:37 +0400

    edit button, add hover

--- Client/src/app/table-artist/table-artist.component.ts
+++ Client/src/app/table-artist/table-artist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-table-music',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableArtistComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  // showButton() {}
 }
