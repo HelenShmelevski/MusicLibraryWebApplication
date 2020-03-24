9aa5d83c90887e2edd5f87fe4831f2d15072ea87
DaryaSova <darya.sova.2016@mail.ru>
2020-03-24 16:20:17 +0400

    валидация полей

--- Client/src/app/auth/login/login.component.ts
+++ Client/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = new FormGroup ({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onSubmit() {
+  console.log((this.form));
   }
 
 }
